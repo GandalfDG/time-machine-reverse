a031625410d6ef7b84031343f83b02e5740c1ebc
Jack Case <jackacase@gmail.com>
2025-04-24 19:31:32 -0400

    f9dasm_export.py outputs comments and labels into .info
    the info file is used as the input to f9dasm to produce
    disassembled code suitable for reassembly.

--- scripts/f9dasm_export.py
+++ scripts/f9dasm_export.py
@@ -7,11 +7,15 @@
 #@runtime PyGhidra
 
 from os import path
-from ghidra.program.flatapi import FlatProgramAPI
-from ghidra.program.model.address import AddressSet, AddressSetViewAdapter
+from ghidra.program.model.address import AddressSet
 
 
-#TODO Add User Code Here
+OPTIONS = ["noaddr",
+           "nohex",
+           "6808",
+           "noconv",
+           "noasc"]
+
 
 state = getState()
 program =  state.getCurrentProgram()
@@ -20,6 +24,7 @@ name = program.getName()
 memory = program.getMemory()
 blocks = memory.getBlocks()
 rom_files = memory.getAllFileBytes()
+symbols = program.getSymbolTable()
 
 def output_code_comments(base_address, last_address, info_file):
 
@@ -44,32 +49,53 @@ def output_code_equates(base_address, last_address, info_file):
     equate_iter = equate_table.getEquates()
     print([(equ.getName(), equ.getValue()) for equ in equate_iter])
     pass 
+
+def output_code_labels(info_file):
+    for symbol in symbols.getSymbolIterator():
+        print(symbol)
+        info_file.write(f"label {symbol.getAddress()} {symbol.getName()}\n")
+    
+    pass
     
 
 print([file.getFilename() for file in rom_files])
 
-info_files = dict()
+files = list()
 
 # for each rom block, get the address offset and write it to the .info file
+info_file = open(path.join("/tmp", f"disassembly.info"), "w")
+
+option_strings = (f"option {option}\n" for option in OPTIONS)
+
+info_file.write("".join(option_strings))
+
 for file in rom_files:
 
     filename = file.getFilename()
     file_base_address = memory.locateAddressesForFileBytesOffset(file, 0)[0]
     file_end_address = file_base_address.add(file.getSize() - 1)
 
-    info_file = open(path.join("/tmp", f"{filename}.info"), "w")
-    info_files[filename] = info_file
+    files.append(file_base_address)
+    files.append(file_end_address)
 
     info_file.write(f"file {filename} {file_base_address}\n")
 
-    # for each instruction in the listing write any comments to the .info file
-    output_code_comments(file_base_address, file_end_address, info_file)
+least_address = min(files)
+most_address = max(files)
+
+print(least_address)
+print(most_address)
+
+# for each instruction in the listing write any comments to the .info file
+output_code_comments(least_address, most_address, info_file)
 
 
 # put each label into the info file
-    output_code_equates(file_base_address, file_end_address, info_file)
+# output_code_equates(file_base_address, file_end_address, info_file)
+
+# output labels into the info file
+output_code_labels(info_file)
 
 # call f9dasm for each file with the generated info file
 
-for file in info_files.values():
-    file.close()
+info_file.close()
